a1f0bb31a5ecde6ce7413b8e708eae918a41fb2b
Xavier Hamel <xavier.hamel.4@ulaval.ca>
2022-06-03 17:02:03 -0400

    Bug fix

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 import {tauri} from './node_modules/@tauri-apps/api/index.js';
 
+console.log(tauri)
 const js = document.querySelector('#javascript');
 js.className = '';
-if (tauri) {
-  const hash = document.location.hash.slice(1);
-  if (hash) {
+//if (tauri) {
+const hash = document.location.hash.slice(1);
+console.log(hash);
+if (hash) {
     js.innerHTML = 'You will be redirected in a moment.';
     tauri.invoke('save_auth_fragment', {fragment: hash});
     tauri.invoke('restore_location').then((location) => {
       document.location.replace(location);
     });
-  } else {
-    js.innerHTML = 'Error: Access Token not found.';
-  }
 } else {
-  document.location.replace(`${document.location.pathname}#no_tauri`);
-  js.innerHTML = 'Error: This page only works in the ELIA Desktop app.';
+js.innerHTML = 'Error: Access Token not found.';
 }
+//} else {
+//document.location.replace(`${document.location.pathname}#no_tauri`);
+//js.innerHTML = 'Error: This page only works in the ELIA Desktop app.';
+//}
